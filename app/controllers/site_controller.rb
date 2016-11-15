0a7f052c7ba7da7d20666cfc2de71052c7bfd894
Belen Linacero <belenlinacero@gmail.com>
2016-11-15 12:01:43 +0100

    last projects

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,8 +2,16 @@ class SiteController < ApplicationController
 	def home
 		render 'home' #name of view template
 	end
-	def say_name
-		@name = params[:name]
-		render 'say_name'
+	def contact
+		render 'contact'
+	end
+	def calculator
+		render 'calculator'
+	end
+	def calculate
+		@num1= params[:first_num].to_f
+		@num2= params[:second_num].to_f
+		@result= @num1 + @num2
+		render 'calculate'
 	end
 end
