3a6cb868dc3ef89e011c8c98176f9ff9e2e92e6f
Belen Linacero <belenlinacero@gmail.com>
2016-11-16 10:23:56 +0100

    Adding new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,10 +2,24 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project. order(created_at: :desc).limit(100)
   end
+
   def show
     @my_project = Project.find_by(id: params[:id])
     unless @my_project
       render "project_not_found"
     end
+  end
+
+  def new
+    @new_project = Project.new
+  end
+
+  def create
+    @new_project = Project.new(
+      :name=> params[:project][:name],
+      :description => params[:project][:description])
+    @new_project.save
+
+    redirect_to '/projects/#{@new_project.id}'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' , to: 'site#home'
   get '/contact', to: 'site#contact'
+
   get '/calculator', to: 'site#calculator'
   post '/calculate', to: 'site#calculate'
+
   get '/projects', to: 'projects#index'
+  get '/projects/new', to: 'projects#new'
+  post '/projects/', to: 'projects#create'
+
   get '/projects/:id', to: 'projects#show'
+
 end
