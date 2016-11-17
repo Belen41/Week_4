a884803ec2ff57f149b897687efd3b6979012025
Belen Linacero <belenlinacero@gmail.com>
2016-11-17 12:34:43 +0100

    Resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,27 @@ Rails.application.routes.draw do
   get '/calculator', to: 'site#calculator'
   post '/calculate', to: 'site#calculate'
 
-  get '/projects', to: 'projects#index'
-  get '/projects/new', to: 'projects#new'
-  post '/projects/', to: 'projects#create'
-
-  get '/projects/:id', to: 'projects#show'
-  get '/projects/:project_id/time_entries', to: 'time_entries#index'
-
-  get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
-  post '/projects/:project_id/time_entries',
-    to: "time_entries#create" , as: :project_time_entries
-
-  get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
-  patch '/projects/:project_id/time_entries/:id/',
-    to: 'time_entries#update',
-    as: 'project_time_entry'
-
-  get '/projects/:project_id/time_entries/:id/update', to: 'time_entries#update'
-
-  delete '/projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
+  resources :projects, only: [:index, :show, :new, :create] do
+    resources :time_entries, except: [:show]
+  end
+  # get '/projects', to: 'projects#index'
+  # get '/projects/new', to: 'projects#new'
+  # post '/projects/', to: 'projects#create'
+  #
+  # get '/projects/:id', to: 'projects#show'
+  # get '/projects/:project_id/time_entries', to: 'time_entries#index'
+  #
+  # get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
+  # post '/projects/:project_id/time_entries',
+  #   to: "time_entries#create" , as: :project_time_entries
+  #
+  # get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
+  # patch '/projects/:project_id/time_entries/:id/',
+  #   to: 'time_entries#update',
+  #   as: 'project_time_entry'
+  #
+  # get '/projects/:project_id/time_entries/:id/update', to: 'time_entries#update'
+  #
+  # delete '/projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
 
 end
